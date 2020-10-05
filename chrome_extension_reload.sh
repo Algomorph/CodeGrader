1fba6349bb745f3807d924e4faedabd6c65fbc81
Gregory Kramida <algomorph@gmail.com>
2020-10-05 14:25:25 -0400

    Add explanatory comment to the reload bash script.

--- chrome_extension_reload.sh
+++ chrome_extension_reload.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# programmatically reload extensions in the Chrome browser, to ease testing changes in chrome
+# (e.g. can be tied to a shortcut in your text editor or IDE)
+# requires the Extension Reloader extension to be installed in Chrome (https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid)
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
