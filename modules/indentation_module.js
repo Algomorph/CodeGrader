ae61bf978bfface5be9f46b3e0ab7ece789fa7d3
Matthew Simmons <msimmon6@terpmail.umd.edu>
2021-04-24 22:03:17 -0400

    Switch statement handling in indent

--- modules/indentation_module.js
+++ modules/indentation_module.js
@@ -48,6 +48,8 @@ let indentation_module = {};
         let isComment = false;
         let isNotAllman = 0;
         let expectedIndent = 0;
+        let isFirstSwitch = false, isSwitch = false;
+        let switchIndent = 0;
 
         // find first indent used and use that as standard
         let singleIndentWidth = 0;
@@ -114,6 +116,10 @@ let indentation_module = {};
 
             // Handle opening and closing braces updating indent size
             if (codeText.trim().indexOf("}") === 0) {
+                if(isSwitch && currentIndentationWidth - 2 * singleIndentWidth <= switchIndent) {
+                    currentIndentationWidth = switchIndent + singleIndentWidth;
+                    isSwitch = isFirstSwitch = false;
+                }
                 currentIndentationWidth -= singleIndentWidth;
                 isPrev = false;
             }
@@ -123,6 +129,10 @@ let indentation_module = {};
                 if (isNotAllman > 0) {
                     isNotAllman--;
                 }
+            }
+
+            if(!isFirstSwitch && codeText.search(/(case\s|default\s|default:)/) !== -1) {
+                currentIndentationWidth -= singleIndentWidth;
             }
 
             if (isNotAllman > 0) {
@@ -183,10 +193,21 @@ let indentation_module = {};
                 isNotAllman = stack.pop(); // Somehow, this fixes nested if's with AND without braces
             }
 
+            if(codeText.search(/(case\s|default\s|default:)/) !== -1) {
+                isFirstSwitch = false;
+                currentIndentationWidth += singleIndentWidth;
+            }
+
+            // I don't want to think about nested switch statements. People who do that are maniacal!
+            if(codeText.search(/switch(\s|\()/) !== -1) {
+                switchIndent = currentIndentationWidth - singleIndentWidth;
+                isSwitch = isFirstSwitch = true;
+            }
+
             // If it doesn't end in a correct delimiter, it's a continuation of the previous line.
-            if (!isPrev && codeText.search(/(for|while|do|else|if)\s/) !== -1
+            if (!isPrev && codeText.search(/(for|while|do|else|if)(\s|\()/) !== -1
                 && codeText.trim().charAt(codeText.trim().length - 1) !== "{" && codeText.trim().charAt(codeText.trim().length - 1) !== ";") {
-                if (codeText.trim().charAt(codeText.trim().length - 1) !== ";" && (codeText.indexOf(")") === codeText.indexOf("(") || (codeText.indexOf(")") !== -1 && codeText.match(/\(/g).length === codeText.match(/\)/g).length))) {
+                if ((codeText.indexOf(")") === codeText.indexOf("(") || (codeText.indexOf(")") !== -1 && codeText.match(/\(/g).length === codeText.match(/\)/g).length))) {
                     if(codeText.indexOf("}") === -1 || codeText.indexOf("{") === -1 || codeText.match(/{/g).length !== codeText.match(/}/g).length) {
                         isPrev = true;
                         isNotAllman++;
