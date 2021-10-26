386a92149ba91ee33fe0538cda4dab858a911a00
Matthew Simmons <msimmon6@terpmail.umd.edu>
2021-10-26 14:34:18 -0400

    Fix to #136

--- options_compiled.js
+++ options_compiled.js
@@ -5975,10 +5975,7 @@ function saveOptions() {
                     status.textContent = '';
                 }, 750);
             });
-        document.getElementById('optionsTextArea').value = beautify(optionsStr, {
-            indent_size: 4,
-            space_in_empty_paren: true
-        });
+
     } catch (error) {
         if (error instanceof SyntaxError) {
             let status = document.getElementById('status');
