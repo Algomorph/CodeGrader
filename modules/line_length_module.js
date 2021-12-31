e1d318909f3a708c6616711c890305b24f6b6a15
Gregory Kramida <algomorph@gmail.com>
2021-12-30 19:07:19 -0500

    Complete & test line length violation code entities & line length module overhaul

--- modules/line_length_module.js
+++ modules/line_length_module.js
@@ -9,13 +9,14 @@ let line_length_module = {};
         enabled;
         lineLengthLimit;
         tabCharacterWidth;
+
         /**
          * Build default options
          * @param {boolean} enabled whether the module is enabled.
          * @param {number} lineLengthLimit maximum allowed line length
          * @param {number} tabCharacterWidth expected tab character width
          */
-        constructor(enabled = false, lineLengthLimit = 80, tabCharacterWidth= 4) {
+        constructor(enabled = false, lineLengthLimit = 80, tabCharacterWidth = 4) {
             this.enabled = enabled;
             this.lineLengthLimit = lineLengthLimit;
             this.tabCharacterWidth = tabCharacterWidth;
@@ -26,39 +27,86 @@ let line_length_module = {};
         return new Options();
     }
 
-    //TODO: make class below inherit from a CodeEntity class. This class should also be used as
-    // prototype for every single thing discovered and stored by code_entity_search entity search.
-    class LineLengthViolation {
+    const moduleColor = "#7c7cd2";
+
+    class LineLengthViolation extends CodeEntity {
+        static options;
+        #lineLength;
+
         /**
          * @param {number} lineLength
          * @param {HTMLTableRowElement} trCodeLine
          */
-        constructor(lineLength, trCodeLine) {
-            this.lineLength = lineLength;
-            this.trCodeLine = trCodeLine;
+        constructor(trCodeLine, lineLength) {
+            super(trCodeLine);
+            this.#lineLength = lineLength;
+        }
+
+        get points() {
+            return -1;
+        }
+
+        get isIssue() {
+            return true;
+        }
+
+        get _labelStyleClass() {
+            return "line-length-exceeded-problem";
+        }
+
+        get _labelName() {
+            return this._tagName;
+        }
+
+        get _toolTip() {
+            return this._defaultMessageText;
+        }
+
+        get _tagName() {
+            return "long line";
+        }
+
+        get _defaultMessageText() {
+            return "Line length (" + this.#lineLength + ") exceeds " + LineLengthViolation.options.lineLengthLimit + " characters.";
+        }
+
+        get _tagColor() {
+            return moduleColor;
         }
     }
 
+    let initialized = false;
+
     /**
-     * Initialize the module: perform code analysis, add relevant controls to the uiPanel.
-     * @param {HTMLDivElement} uiPanel main panel where to add controls
-     * @param {Map.<string, CodeFile>} fileDictionary
-     * @param {Options} options
+     * Initialize the module
+     * @param {{moduleOptions : {line_length_module: Options}}} global_options
      */
-    this.initialize = function (uiPanel, fileDictionary, options) {
-        if (!options.enabled) {
+    this.initialize = function (global_options) {
+        this.options = global_options.moduleOptions.line_length_module;
+
+        if (!this.options.enabled) {
+            return;
+        }
+        LineLengthViolation.options = this.options;
+        /** @type {Array.<LineLengthViolation>} */
+        this.lineLengthViolations = [];
+        initialized = true;
+    }
+
+    /**
+     * Perform code analysis & discover code entities of interest
+     * @param {Map.<string, CodeFile>} codeFileDictionary
+     */
+    this.processCode = function (codeFileDictionary) {
+        if (!this.options.enabled) {
             return;
         }
 
-        $(uiPanel).append("<h3 style='color:#7c7cd2'>Line Length</h3>");
-
-        const lineLengthViolations = [];
-
-        for (const codeFile of fileDictionary.values()) {
+        for (const codeFile of codeFileDictionary.values()) {
             const startTrCodeLine = codeFile.trCodeLines[0];
             const characterWidth = getMonospaceCharacterWidth(startTrCodeLine);
             const allowedPixelWidthOfCode = (options.lineLengthLimit + 1) * characterWidth;
-            const codeCellLeftOffset =  getCodeCellLeftOffsetPixels(startTrCodeLine);
+            const codeCellLeftOffset = getCodeCellLeftOffsetPixels(startTrCodeLine);
             const cutoffLineLeftOffset = allowedPixelWidthOfCode + codeCellLeftOffset;
             const cutoffLineHeight = getCodeAreaHeightFromTrCodeLine(startTrCodeLine);
 
@@ -67,24 +115,39 @@ let line_length_module = {};
             $.each(codeFile.trCodeLines, function (codeLineIndex, trCodeLine) {	// iterates each line of code below
                 const codeLine = codeFile.codeLines[codeLineIndex];
                 const lineLength = getLineCharacterWidth(codeLine);
-                if(lineLength > options.lineLengthLimit){
-                    lineLengthViolations.push(new LineLengthViolation(lineLength, trCodeLine))
+                if (lineLength > this.options.lineLengthLimit) {
+                    this.lineLengthViolations.push(new LineLengthViolation(trCodeLine, lineLength))
                 }
             });
         }
-
-
-
-        for(const lineLengthViolation of lineLengthViolations){
-            const defaultMessageText = "Line length (" + lineLengthViolation.lineLength + ") exceeds " + options.lineLengthLimit + " characters.";
-            $(uiPanel).append(makeLabelWithClickToScroll("Long line", lineLengthViolation.trCodeLine,
-                "line-length-exceeded-problem", defaultMessageText ));
-            addCodeTagWithComment(
-                lineLengthViolation.trCodeLine,
-                "long line", defaultMessageText, "#7c7cd2"
-            );
+    }
+    /**
+     * Add all information collected so far by the module to the UI panel.
+     * @param {HTMLDivElement} uiPanel
+     */
+    this.addInfoToUiPanel = function (uiPanel) {
+        if (!this.options.enabled) {
+            return;
         }
+        $(uiPanel).append("<h3 style='color:" + moduleColor + "'>Student Tests</h3>");
+        for (const lineLengthViolation of this.lineLengthViolations) {
+            lineLengthViolation.addAsLabelToPanel(uiPanel);
+            lineLengthViolation.addAsCodeTagWithDefaultComment();
+        }
+    }
 
+    /**
+     * Return all CodeEntities thus far extracted from the code.
+     * @returns {Array.<CodeEntity>}
+     */
+    this.getCodeEntities = function () {
+        if (!this.options.enabled) {
+            return [];
+        }
+        if (!initialized) {
+            throw ("Module not initialized. Please call the initialize function first.");
+        }
+        return this.lineLengthViolations;
     }
 
 }).apply(line_length_module);
